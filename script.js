0d18980bc3a6b3b4a7412bc3aa3c920ab8e995bd
Joseph carabajo <joemcara@espol.edu.ec>
2023-07-15 01:32:08 -0500

    version 1.0.0

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("btnRedirect").addEventListener("click", function() {
+
+    const newP = document.createElement('p');
+    newP.innerText = 'Sabia que dirias que si vistase guapa que vamos a loquear';
+    document.getElementById('action2').appendChild(newP)
+
+    const newh2 = document.createElement('h5');
+    newh2.innerText = 'Te dedico esta canciocita \n Te amo guapa'
+    document.getElementById('action').appendChild(newh2)
+
+    const newimg = document.createElement('img');
+    newimg.src = 'https://i.pinimg.com/564x/7c/e7/3c/7ce73c4e4e8171e4647ce1441aa23a8b.jpg'
+    document.querySelectorAll('.imague')[1].appendChild(newimg)
+
+
+    const newbtn = document.createElement('button');
+    newbtn.innerText = 'Ir a la musica <3'
+    newbtn.addEventListener('click',function(){
+        window.location.href = "https://www.youtube.com/watch?v=trUZwHvMflU";
+    })
+    document.getElementById('action').appendChild(newbtn)
+    document.getElementById("btnRedirect").disabled = true
+    document.getElementById('btnRedirect').innerText = 'Es una cita :D'
+});
+
+
